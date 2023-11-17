2303fc45f68079c66c88cb1ede0c8176d1f2b4d3
Hossein <abdollahzadeh.hossein@gmail.com>
2023-11-17 12:42:19 +0330

    Create models

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,15 +12,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'username',
     ];
 
     /**
@@ -29,16 +22,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
